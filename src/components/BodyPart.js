15de76c383aefef2ea9b509f41bb6fc665545af0
Anas Youssef <anasyoussef649@gmail.com>
2022-07-31 21:29:36 +0200

    agiza

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -3,7 +3,6 @@ import { Stack, Typography } from "@mui/material";
 import icon from "../assets/icons/gym.png";
 
 const BodyPart = ({ item, setBodyPart, bodyPart }) => {
-  console.log(item);
   return (
     <Stack
       type="button"
