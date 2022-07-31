a2bc1d3e2fd0aa19bfbd41bb49635c43b483e444
Anas Youssef <anasyoussef649@gmail.com>
2022-07-31 21:16:21 +0200

    final

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -145,7 +145,7 @@ const Services = () => {
         <Box
           sx={{
             width: { lg: "800px", md: "500px", sm: "300px" },
-            padding: "10px",
+
             borderRadius: "20px",
             display: "flex",
             alignItems: "center",
